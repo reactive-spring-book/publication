052c5bd20149aab7c1c5db3261d67d3acc13a1a9
Josh Long <josh@joshlong.com>
2018-09-30 23:30:12 -0700

    up

--- setup-env.sh
+++ setup-env.sh
@@ -14,6 +14,7 @@ sudo -E apt-get -yq --no-install-suggests --no-install-recommends install imagem
 # sudo apt-get install software-properties-common
 # sudo apt-add-repository -y ppa:rael-gc/rvm
 # sudo apt-get update
+
 # sudo apt-get install rvm
 rvm install "ruby-2.3.1"
 gem install bundler -v '< 2'

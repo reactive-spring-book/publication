8300a2b15d2830f699fbdd91dbe026239d7f2492
Josh Long <josh@joshlong.com>
2018-09-30 23:04:03 -0700

    up

--- setup-env.sh
+++ setup-env.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Ghostscript 
+gs || sudo apt install ghostscript
+
 # RVM ..  
+
 sudo apt-get install software-properties-common
 sudo apt-add-repository -y ppa:rael-gc/rvm
 sudo apt-get update
 sudo apt-get install rvm
 rvm install "ruby-2.3.1"
 gem install bundler -v '< 2'
+
 
 

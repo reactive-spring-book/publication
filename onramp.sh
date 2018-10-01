f7dea7438cb026f5f063e44a996541ebe000ff84
Josh Long <josh@joshlong.com>
2018-09-30 22:24:26 -0700

    polish

--- onramp.sh
+++ onramp.sh
@@ -7,7 +7,7 @@ echo "the book clone will be at ${D}"
 if [ ! -d $D ] ; then 
 	msg="cloned the http://github.com/joshlong/reactive-spring-book into ${D}. \n\nNow, going to attempt a book build..."
 	mkdir -p $(dirname $D)
-	git clone https://${RSB_GITHUB_TOKEN}@github.com/joshlong/reactive-spring-book.git $D &&  echo  $msg \
+	git clone https://${RSB_GITHUB_USERNAME}:${RSB_GITHUB_PASSWORD}@github.com/joshlong/reactive-spring-book.git $D &&  echo  $msg \
 		||  echo "couldn't clone https://${RSB_GITHUB_TOKEN}@github.com/joshlong/reactive-spring-book.git .."
 fi 
 

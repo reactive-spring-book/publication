f5fa7829a13b140da553dd0d48305050a996b92b
Josh Long <josh@joshlong.com>
2018-09-30 23:19:24 -0700

    up

--- setup-env.sh
+++ setup-env.sh
@@ -6,12 +6,10 @@ sudo add-apt-repository -y ppa:moti-p/cc
 sudo apt-get update
 sudo apt-get -y --reinstall install ghostscript
 sudo apt-get -y --reinstall install gs-esp
-# sudo apt-get -y --reinstall install imagemagick
-gs 
+sudo apt-get -y --reinstall install imagemagick
+
 
 # RVM ..  
-echo "exiting!!!!!!!!!"
-exit 
 sudo apt-get install software-properties-common
 sudo apt-add-repository -y ppa:rael-gc/rvm
 sudo apt-get update

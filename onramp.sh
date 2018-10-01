deb1c795bb9384d4f77fe5eaaaa7624e112ce646
Josh Long <josh@joshlong.com>
2018-09-30 23:06:58 -0700

    up

--- onramp.sh
+++ onramp.sh
@@ -8,7 +8,7 @@ D=${TMPDIR:-${TRAVIS_TMPDIR:-/tmp}}/book
 echo "the book clone will be at ${D}"
 
 if [ ! -d $D ] ; then 
-	msg="cloned the http://github.com/joshlong/reactive-spring-book into ${D}. \n\nNow, going to attempt a book build..."
+	msg="cloned the http://github.com/joshlong/reactive-spring-book into ${D}.."
 	mkdir -p $(dirname $D)
 	URI=https://${RSB_GITHUB_TOKEN}@github.com/joshlong/reactive-spring-book.git 
 	git clone $URI $D && echo $msg || echo "couldn't clone $URI .."

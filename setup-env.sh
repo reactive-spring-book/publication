e0f2e95448a067b1e01523b101b4e80f67193e60
Josh Long <josh@joshlong.com>
2018-09-30 23:41:58 -0700

    up

--- setup-env.sh
+++ setup-env.sh
@@ -2,19 +2,16 @@
 
 # Ghostscript 
 #sudo -E apt-get -yq --no-install-suggests --no-install-recommends install imagemagick ghostscript rvm 
+sudo apt-add-repository -y ppa:rael-gc/rvm
+sudo add-apt-repository -y ppa:moti-p/cc
+sudo apt-get update
 
-# sudo add-apt-repository -y ppa:moti-p/cc
-# sudo apt-get update
-# sudo apt-get -y --reinstall install ghostscript
-# sudo apt-get -y --reinstall install gs-esp
-# sudo apt-get -y --reinstall install imagemagick
+sudo apt-get -y --reinstall install ghostscript
+sudo apt-get -y --reinstall install gs-esp
+sudo apt-get -y --reinstall install imagemagick
+sudo apt-get -y --reinstall software-properties-common
 
-# RVM ..  
-# sudo apt-get install software-properties-common
-# sudo apt-add-repository -y ppa:rael-gc/rvm
-# sudo apt-get update
 
-# sudo apt-get install rvm
 rvm install "ruby-2.3.1"
 gem install bundler -v '< 2'
 

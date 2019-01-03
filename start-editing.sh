61ad4f3de6f43b604a8a9cd982f2c804e439746c
joshlong <josh@joshlong.com>
2019-01-03 14:51:40 -0800

    up

--- start-editing.sh
+++ start-editing.sh
@@ -6,10 +6,13 @@ start=$( cd `dirname $0` && pwd	 )
 echo "initializing from ${start} "
 
 cat ${start}/repositories.txt  | while read l ; do
- #do git clone $l ; 
+ 
  d=$( echo $l |  cut -f2 -d\/ | cut -f1 -d. ) 
- dir_to_create=${start}/../$d
+ 
  echo "Processing $d" 
+
+ 
+ dir_to_create=${start}/../$d
  
  if [[ -e  $dir_to_create ]] ; then 
  	echo "WARN: ${dir_to_create} aleady exists." # if you want this to be freshly initialized then check in your work and delete the directory 
